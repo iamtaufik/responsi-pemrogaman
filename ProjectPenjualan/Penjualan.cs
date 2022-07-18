8106d6bb3a99ee130a43c5d25f414b40ea415c3f
muhammadtaufikhidayat <muhtopik07@gmail.com>
2022-07-18 21:44:25 +0700

    Penjelasan Kode Program

--- ProjectPenjualan/Penjualan.cs
+++ ProjectPenjualan/Penjualan.cs
@@ -5,17 +5,22 @@ using System.Text;
 
 namespace ProjectPenjualan
 {
+    // Deklarasi Class Penjualan dengan Acces Modifier Public
     public class Penjualan
     {
-        // PERINTAH: lengkapi property class penjualan, sesuai petunjuk soal
+        // Property Nota yang menampung tipe data String dengan Acces Modifier Public
         public string Nota { get; set; }
 
+        // Property Tanggal yang menampung tipe data String dengan Acces Modifier Public
         public string Tanggal { get; set; }
 
+        // Property Costumer yang menampung tipe data String dengan Acces Modifier Public
         public string Costumer { get; set; }
 
+        // Property Jenis yang menampung tipe data String dengan Acces Modifier Public
         public string Jenis { get; set; }
 
+        // Property Total yang menampung tipe data Double dengan Acces Modifier Public
         public double Total { get; set; }
     }
 }
--- ProjectPenjualan/Program.cs
+++ ProjectPenjualan/Program.cs
@@ -14,26 +14,48 @@ namespace ProjectPenjualan
         {
             Console.Title = "Responsi UAS Matakuliah Pemrograman";
 
+            // Perulangan menggunakan While
             while (true)
             {
+                // Memanggil Fungsi TampilMenu
                 TampilMenu();
 
                 Console.Write("\nNomor Menu [1..4]: ");
+
+                // menyimpan input yang dimasukkan user ke variable nomorMenu yang bertipe data integer
                 int nomorMenu = Convert.ToInt32(Console.ReadLine());
 
+                // Percabangan menggunakan switch case
                 switch (nomorMenu)
                 {
+                    // Jika nomerMenu bernilai 1 maka akan meenjalankan blok berikut
                     case 1:
+                        
+                        // Menjalankan fungsi TambahPenjualan
                         TambahPenjualan();
+
+                        // Keluar dari switch case
                         break;
 
+                    // Jika nomerMenu bernilai 2 maka akan meenjalankan blok berikut
                     case 2:
+
+                        // Menjalankan fungsi HapusPenjualan
                         HapusPenjualan();
+
+                        // Keluar dari switch case
                         break;
 
+                    // Jika nomerMenu bernilai 3 maka akan meenjalankan blok berikut
                     case 3:
+
+                        // Menjalankan fungsi TampilPenjualan
                         TampilPenjualan();
-                        return;
+
+                        // Keluar dari switch case
+                        break;
+
+                    // Jika nomerMenu bernilai 3 maka akan meenjalankan blok berikut
                     case 4: // keluar dari program
                         return;
                     default:
@@ -42,93 +64,181 @@ namespace ProjectPenjualan
             }
         }
 
+        // Deklarasi Fungsi TampilMenu
         static void TampilMenu()
         {
+            // Membersihkan Layar Console 
             Console.Clear();
+
+            // Memberikan judul Console
             Console.Title = "Responsi Pemrograman";
+
+            // Menampilkan sebuah string tanpa pindah baris
             Console.Write(" Pilih Menu :");
+
+            // Menampilkan sebuah string dengan pindah baris
             Console.WriteLine();
+
+            // Menampilkan sebuah string tanpa pindah baris
             Console.Write("1.Tambah Data Penjualan");
+
+            // Menampilkan sebuah string dengan pindah baris
             Console.WriteLine();
+
+            // Menampilkan sebuah string tanpa pindah baris
             Console.Write("2.Hapus Data Penjualan");
+
+            // Menampilkan sebuah string dengan pindah baris
             Console.WriteLine();
+
+            // Menampilkan sebuah string tanpa pindah baris
             Console.Write("3.Tampilkan Penjualan");
+
+            // Menampilkan sebuah string dengan pindah baris
             Console.WriteLine();
+
+            // Menampilkan sebuah string tanpa pindah baris
             Console.Write("4.Keluar");
+
+            // Menampilkan sebuah string dengan pindah baris
             Console.WriteLine();
         }
 
+        // Deklarasi Fungsi TambahPenjualan
         static void TambahPenjualan()
         {
+            // Membersihkan layar Console
             Console.Clear();
 
-             Console.WriteLine("Tambah Penjualan");
+            // Menampilkan sebuah string dengan pindah baris
+            Console.WriteLine("Tambah Penjualan");
+
+            // Menampilkan sebuah string tanpa pindah baris
             Console.Write("Nota : ");
+
+            // menyimpan input yang dimasukkan user ke variable nota yang bertipe data string
             string nota = Console.ReadLine();
+
+            // Menampilkan sebuah string tanpa pindah baris
             Console.Write("Tanggal : ");
+
+            // menyimpan input yang dimasukkan user ke variable tanggal yang bertipe data string
             string tanggal = Console.ReadLine();
+
+            // Menampilkan sebuah string tanpa pindah baris
             Console.Write("Costumer : ");
+
+            // menyimpan input yang dimasukkan user ke variable costumer yang bertipe data string
             string costumer = Console.ReadLine();
+
+            // Menampilkan sebuah string tanpa pindah baris
             Console.Write("Jenis [T/K] : ");
+
+            // menyimpan input yang dimasukkan user ke variable jenis yang bertipe data string
             string jenis = Console.ReadLine();
+
+            // Pengkondisian jika niali variable jenis sama dengan string T maka eksekusi blok berikut
             if (jenis == "T")
             {
+
+                // Assigment variable jenis dengan string Tunai
                 jenis = "Tunai";
             }
+
+            // Jika bukan maka esekusi blok berikut
             else
             {
+
+                // Assigment nilai variable jenis dengan string Kredit
                 jenis = "Kredit";
             }
+
+            // Menampilkan sebuah string tanpa pindah baris
             Console.Write("Total Nota : ");
+
+            // menyimpan input yang dimasukkan user ke variable total yang bertipe data double
             double total = Convert.ToDouble(Console.ReadLine());
 
             daftarPenjualan.Add(new Penjualan() { Nota = nota, Tanggal = tanggal, Costumer = costumer, Jenis = jenis, Total = total });
 
+            // Menampilkan sebuah string dengan pindah baris
             Console.WriteLine("\nTekan ENTER untuk kembali ke menu");
+            
+            // Membaca Key yang diinputkan user untuk melanjutkan program
             Console.ReadKey();
         }
 
+        // Deklarasi fungsi HapusPenjualan
         static void HapusPenjualan()
         {
+            // Membersihkan layar console
             Console.Clear();
 
+            // Menampilkan sebuah string dengan pindah baris
             Console.WriteLine("Hapus Data Penjualan");
-            string hapus;
+            
+            // Menampilkan sebuah string tanpa pindah baris
             Console.WriteLine("Nota : ");
-            hapus = Console.ReadLine();
+
+            // menyimpan input yang dimasukkan user ke variable hapus yang bertipe data string
+            string hapus = Console.ReadLine();
+
+            // deklarasi variable x dengan tipe data integer yang mempunyai value 0
             int x = 0;
             foreach (Penjualan penjualan in daftarPenjualan)
             {
                 if (hapus == penjualan.Nota)
                 {
                     daftarPenjualan.RemoveAt(x);
+
+                    // Menampilkan sebuah string dengan pindah baris
                     Console.WriteLine("Data Penjualan Berhasil Dihapus");
+
+                    // Keluar dari perulangan foreach
                     break;
 
                 }
+
+                // Incerment value dari variable X
                 x++;
+
+                // Menampilkan sebuah string dengan pindah baris
                 Console.WriteLine("Nota tidak ditemukan");
             }
 
-
+            // Menampilkan sebuah string dengan pindah baris
             Console.WriteLine("\nTekan ENTER untuk kembali ke menu");
+
+            // Membaca Key yang diinputkan user untuk melanjutkan program
             Console.ReadKey();
         }
 
+        // Deklarasi fungsi TampilPenjualan
         static void TampilPenjualan()
         {
+            // Membersihkan layar Console
             Console.Clear();
 
+            // Menampilkan sebuah string dengan pindah baris
             Console.WriteLine("Data Penjualan");
+
+            // Deklarasi variable no dengan tipe data Integer yang mempunyai value 1
             int no = 1;
             foreach (Penjualan penjualan in daftarPenjualan)
             {
+
+                // Menampilkan sebuah string dengan pindah baris
                 Console.WriteLine("{0}. {1}, {2}, {3}, {4}, {5}",
                 no, penjualan.Nota, penjualan.Tanggal, penjualan.Costumer, penjualan.Jenis, penjualan.Total);
+                
+                // Incerment nilai variable no
                 no++;
             }
 
+            // Menampilkan sebuah string dengan pindah baris
             Console.WriteLine("\nTekan enter untuk kembali ke menu");
+
+            // Membaca Key yang diinputkan user untuk melanjutkan program
             Console.ReadKey();
         }
     }
